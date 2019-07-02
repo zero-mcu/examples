d0d1c4f94d4bf2306c6d90de0b652229ea51e65c
yuanle <yuanle@gzseeing.com>
2019-07-02 09:11:02 +0800

    1. use manifest to update zero sdk, if sdk not exist

--- stm32-template/README.md
+++ stm32-template/README.md
@@ -1,4 +1,1 @@
-## warship
-
-
-
+# warship
--- stm32-template/build.sh
+++ stm32-template/build.sh
@@ -1,10 +1,91 @@
 #!/bin/bash
 
-source $ZERO_MCU_HOME/scripts/env.sh
 project_dir=$(cd "$(dirname "$0")"; 'pwd')
 echo "project_dir=$project_dir"
+zero_workspace=$(cd "$project_dir/.."; 'pwd')
+zero_sdk_name="zero-mcu"
 build_dir=$project_dir/build
 build_type="Debug"
+sync_type="none"
+
+
+clean_project()
+{
+    echo "clean project"
+    rm -rf $build_dir
+    exit 1
+}
+
+Usage()
+{
+    echo "----------------------------------------
+Usage:
+    sh build.sh [debug|release] --sync
+    sh build.sh clean
+------------------------------------------"
+    exit 1
+}
+
+if [ "$#" -eq 1 ] && [ "$1" = "-h" ]; then
+    Usage
+elif [ "$#" -ge 1 ]; then
+    if [ "$1" != "--sync" ]; then
+        if [ "$1" = "debug" ]; then
+            build_type="Debug"
+        elif [ "$1" = "release" ]; then
+            build_type=$1
+        elif [ "$1" = "clean" ]; then
+            clean_project
+        else
+            Usage
+        fi
+    fi
+
+    for arg in $*
+    do
+        if [ $arg = "--sync" ]; then
+            sync_type="sync"
+        fi
+    done
+fi
+
+
+sync_zero_path()
+{
+    echo "sync zero path ..."
+    echo "Check zero sdk path: $ZERO_MCU_HOME"
+    if [ -d $ZERO_MCU_HOME/.repo ]; then
+        if [ $sync_type = "sync" ]; then
+            echo "Enter $ZERO_MCU_HOME"
+            pushd $ZERO_MCU_HOME
+            repo sync
+            popd
+            echo "Exit"
+        fi
+    else
+        echo "No zero sdk found, install..."
+        . $HOME/.profile
+        if [ ! -d $zero_workspace ]; then
+            mkdir -p $zero_workspace
+        fi
+        echo "Enter $zero_workspace"
+        cd $zero_workspace
+        if [ ! -d $zero_workspace/manifest ]; then
+            echo "No manifest found, clone from https://github.com/zero-mcu/manifest.git"
+            git clone https://github.com/zero-mcu/manifest.git
+        else
+            echo "Enter $zero_workspace/manifest"
+            cd $zero_workspace/manifest
+            git pull
+            sh install.sh $zero_sdk_name --install $zero_workspace
+        fi
+    fi
+}
+
+source $HOME/.profile
+sync_zero_path
+source $HOME/.profile
+source $ZERO_MCU_HOME/scripts/env.sh
 
 build_project()
 {
@@ -19,43 +100,7 @@ build_project()
     fi
     make
 }
-
-
-clean_project()
-{
-    echo "clean project"
-    rm -rf $build_dir
-    exit 1
-}
-
-Usage()
-{
-    echo "----------------------------------------
-Usage:
-    sh build.sh [debug|release]
-    sh build.sh clean
-------------------------------------------"
-    exit 1
-}
-
-if [ "$#" -eq 1 ] && [ "$1" = "-h" ]; then
-    Usage
-fi
-
-if [ "$#" -eq 1 ]; then
-    if [ "$1" = "clean" ]; then
-        clean_project
-    elif [ "$1" = "debug" ]; then
-        build_type="Debug"
-    elif [ "$1" = "release" ]; then
-        build_type="Release"
-    else
-        Usage
-    fi
-elif [ "$#" -gt 1 ]; then
-    Usage
-fi
-
 build_project
+
 
 
